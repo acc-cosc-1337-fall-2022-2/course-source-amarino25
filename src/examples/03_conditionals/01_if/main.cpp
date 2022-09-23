cf14737ec79c77ac059309fc325213a6a7dab834
amarino25 <amanda.marino2@g.austincc.edu>
2022-09-22 20:57:36 -0500

    updates

--- src/examples/03_conditionals/01_if/main.cpp
+++ src/examples/03_conditionals/01_if/main.cpp
@@ -8,16 +8,26 @@ int main()
 {
 	//create a bool variable named even
 	//create an int variable named value
+	auto over_time = false;
+	auto hours = 0;
 
 	//prompt user for a keyboard number
 	//assign the number to the value variable
+	cout<< "Enter hours: ";
+	cin>>hours;
 
 	//call the is_even function with value as its argument
 	//and assign its return value to the even variable
-	
-
+	over_time= is_overtime(hours);
 	//display even if number is even
-
-
+	if(over_time)
+	{
+		cout<<"Hours: "<<hours<<" is overtime\n";
+	}
+	else
+	{
+		cout<<"Hours: "<<hours<<" is not overtime\n";
+	}
+	
 	return 0;
 }
--- src/examples/03_conditionals/03_if_else_if/main.cpp
+++ src/examples/03_conditionals/03_if_else_if/main.cpp
@@ -1,6 +1,7 @@
 //write include statements
-#include "if_else.h"
+#include "if_else_if.h"
 #include<iostream>
+
 
 //write cin and cout using statements
 using std::cout;
@@ -13,6 +14,12 @@ Pass the year to get_generation function and display function result.
 int main() 
 {
 	
+	auto year =0;
+	cout<<"Enter a year: ";
+	cin>>year;
 	
+	auto generation = get_generation(year);
+	cout<<"Generation is: "<<generation;
+
 	return 0;
 }
