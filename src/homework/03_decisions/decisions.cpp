3d33dc98a9495d111e625bb2998fc9ac519933f3
amarino25 <amanda.marino2@g.austincc.edu>
2022-09-23 00:57:25 -0500

    hw 3 w test cases

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,75 @@
 //write include statement for decisions header
+#include "decisions.h"
 
+using std::string;
+//returns 'A' when value is between 90 and 100
+//returns 'B' when value is between 80 and 89
+//returns 'C' when value is between 70 and 79
+//returns 'D' when value is between 60 and 69
+//returns 'F' when value is between 00 and 59
 
 //Write code for function(s) code here
+std::string get_letter_grade_using_if(int grade)
+{
+    string letterGrade;
+    if(grade >= 90 && grade <= 100)
+    {
+        letterGrade = "A";
+    }
+    else if(grade >= 80 && grade <= 89)
+    {
+        letterGrade = "B";
+    }
+    else if(grade >= 70 && grade <= 79)
+    {
+        letterGrade = "C";
+    }
+    else if(grade >= 60 && grade <= 69)
+    {
+        letterGrade = "D";
+    }
+    else if(grade >= 00 && grade <= 59)
+    {
+        letterGrade = "F";
+    }
+    else
+    {
+        letterGrade = "Number is out of range";
+    }
+    
+    return letterGrade;
+}
+// Wrtie code using switch
+//returns 'A' when value is between 90 and 100
+//returns 'B' when value is between 80 and 89
+//returns 'C' when value is between 70 and 79
+//returns 'D' when value is between 60 and 69
+//returns 'F' when value is between 00 and 59
+
+std::string get_letter_grade_using_switch(int grade)
+{
+    std:string letterGrade;
+
+    switch(grade)
+    {
+        case 95:
+            letterGrade ="A";
+            break;
+        case 85:
+            letterGrade ="B";
+            break;
+        case 75:
+            letterGrade ="C";
+            break;
+        case 65:
+            letterGrade ="D";
+            break;
+        case 50:
+            letterGrade ="F";
+            break;
+        default:
+            letterGrade = "Number is out of range \n";
+            break;
+    }
+    return letterGrade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,54 @@
 //write include statements
+#include "decisions.h"
+#include <iostream>
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	std::cout<<"1. Letter grade using if \n";
+	std::cout<<"2. Letter grade using switch \n";
+	std::cout<<"3. Exit \n";
+	int choice;
+	std::cin>>choice;
+
+	switch(choice)
+	{
+		case 1:
+		case 2:{
+		auto grade = 0;
+		cout<<"Enter a number grade: ";
+		cin>>grade;
+
+		if (choice == 1) {
+
+		auto letterGrade = get_letter_grade_using_if(grade);
+
+		cout<<"Letter grade is: "<<letterGrade << "\n";
+		} else if (choice == 2) {
+
+		auto letterGrade = get_letter_grade_using_switch(grade);
+
+		cout<<"Letter grade is: "<<letterGrade << "\n";
+		}
+}
+	case 3:
+	cout<< "Goodbye";
 	return 0;
+	}
+
+	// Enter 1 2 3
+	// cin >> choice
+	// switch(choice)
+	// case 1 : using if
+	//	Letter Grade: 
+	// 	cin >> grade
+	//	using if
+	// exit
+	// 
+	// case 2 : using switch
+	// case 3 : exit
+
+	
 }
