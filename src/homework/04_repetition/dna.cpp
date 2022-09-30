35dc90c58d998cc284ad200138d7ff5e615c6915
amarino25 <amanda.marino2@g.austincc.edu>
2022-09-30 00:46:13 -0500

    hw 4 w test cases

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,39 @@
 //add include statements
+#include<iostream>
+#include "dna.h"
+
+using std::cout;
+using std::cin;
 
 //add function(s) code here
+int factorial(int num)
+{
+    int factorial = 0;
+    while(num>0)
+    {
+        factorial += num * num;
+        num--;
+    
+    }
+    return factorial;
+}
+
+int gcd(int num1, int num2)
+{
+    while (num1 != num2)
+    {
+        if(num1 < num2) 
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        
+        if (num1 > num2) 
+        {
+            num1 -= num2;
+        }
+    }
+        
+    return num1;
+}
