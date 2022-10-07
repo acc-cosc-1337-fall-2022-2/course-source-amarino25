77182313c53333e6086673314db830fa225e1cc3
amarino25 <amanda.marino2@g.austincc.edu>
2022-10-07 00:40:45 -0500

    hw 5 with test cases

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,71 @@
 //add include statements
+#include<iostream>
+#include "func.h"
 
 //add function code here
+using std::cout;
+using std::cin;
+
+double get_gc_content(const string& dna)
+{
+    double total_chars = (double) dna.length();
+    int total_gc = 0;
+
+    for (int i = 0; i < dna.length(); i++)
+    {
+        if ('C' == dna[i] || 'G' == dna[i]) 
+        {
+            total_gc++;
+        }        
+    }
+
+    return total_gc / total_chars;
+}
+
+string get_dna_complement(string dna)
+{
+    string complement = "";
+
+    for (int i = 0; i < dna.length(); i++)
+    {
+
+        switch(dna[i]) {
+            case 'A':
+            {
+                complement = complement + 'T';
+            }
+            break;
+            case 'T':
+            {
+                complement = complement + 'A';
+            }
+            break;
+            case 'G':
+            {
+                complement = complement + 'C';
+            }
+            break;
+            case 'C':
+            {
+                complement = complement + 'G';
+            }
+            break;
+            default:
+            break;
+        }
+    }
+
+    return get_reverse_string(complement);
+}
+
+string get_reverse_string(string dna)
+{
+    string reversed = "";
+
+    for (int i = dna.length() - 1; i >= 0; i--)
+    {
+        reversed = reversed + dna[i];
+    }
+
+    return reversed;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -7,8 +7,57 @@ and display them to screen.
 Program continues until user decides to exit.
 
 */
+
+#include<iostream>
+#include "func.h"
+
+using std::cout;
+using std::cin;
+using std::string;
+
+
 int main() 
 {
+	char exit = 'N';
 
+	do
+	{
+		int choice;
+		std::cout<<"1. Get GC Content \n";
+		std::cout<<"2. Get DNA Complement \n";
+		std::cout<<"3. Exit \n";
+		std::cout<<"\n Choose a function: ";
+		std::cin>>choice;
+
+		switch(choice)
+		{
+			case 1:
+			{
+				const string& dna;
+				std::cout<<" Enter a DNA string: ";
+				std::cin>>;
+				
+				
+			}
+			break;
+			case 2:
+			{
+				string dna;
+				std::cout<<"Enter a DNA string: ";
+				std::cin>>;
+				;
+			}
+			break;
+			case 3:
+			{
+				std::cout<<"Are you sure you want to exit? (Y/N: ";
+				std::cin>>exit;
+			}
+			break;
+			default:
+			break;
+		}
+	}   while(exit != 'Y');
+	
 	return 0;
 }
