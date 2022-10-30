b221795b923bf516649ccffda154bf5946c19c9c
amarino25 <amanda.marino2@g.austincc.edu>
2022-10-29 20:06:16 -0500

    homework 6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,45 @@
+#include"tic_tac_toe.h"
+#include<iostream>
+#include<string>
+
+using std::string;
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	tic_tac_toe game;
+	string first_player;
+	int position;
+	bool loop_againl
 
+	do
+	{
+		cout<<"Enter X or O:";
+		cin>>first_player;
+	while(first_player!="X"&&first_player!="O")
+	{
+		cout<<"Invalid try again"<<"\n";
+		cout<<"Enter X or O:";
+		cin>> first_player 
+	}
+	start_game(first_player);
+
+	do
+	{
+		display_board();
+		cout<<"Enter a number form 1 to 9:";
+		cin>>position;
+
+		mark_board(position);
+	
+	} 
+	while(game_over()==false);
+	display_board();
+
+	cout<<"Play again(1 for yes 0 for no)";
+	cin>>loop_again;
+	
+	
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe.cpp
@@ -1,1 +1,72 @@
 //cpp
+#include "tic_tac_toe.h"
+#include<iostream>
+#include<string>
+#include<vector>
+
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+
+bool tic_tac_toe::game_over()
+{
+    return check_board_full();
+}
+
+void tic_tac_toe::start_game(std::string first_player)
+{
+    player = first_player;
+    clear_board();
+}
+
+void tic_tac_toe::mark_board(int position)
+{
+    pegs.at(position-1) = player;
+    set_next_player();
+}
+
+std::string tic_tac_toe::get_player()const
+{
+    return player;
+}
+
+void tic_tac_toe:: display_board() const
+{
+    string output = "";
+    output = pegs.at(0) + "|" + pegs.at(1) + "|" + pegs.at(2) + "\n"
+           + pegs.at(3) + "|" + pegs.at(4) + "|" + pegs.at(5) + "\n"
+           + pegs.at(6) + "|" + pegs.at(7) + "|" + pegs.at(8) + "\n";
+               
+    cout<<output;
+}
+
+void tic_tac_toe::set_next_player()
+{
+    if(player =="X")
+    {
+        player = "O";
+    }
+    else
+    {
+        player = "X";
+    }
+    
+}
+
+bool tic_tac_toe::check_board_full()
+{
+    for(int pos = 0; pos < pegs.size(); pos++) {
+        if ( pegs.at(pos) == " ")
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+void tic_tac_toe::clear_board()
+{
+    pegs = {" "," "," "," "," "," "," "," "," "};
+}
